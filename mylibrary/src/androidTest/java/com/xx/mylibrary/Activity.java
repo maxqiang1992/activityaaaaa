f1cf4a438d0d8cab6fe1f3f2ee9c6b7dc3ca06d1
xiongxiang <18727898593@163.com>
2021-05-24 02:20:04 +0800

    1

---
+++ mylibrary/src/androidTest/java/com/xx/mylibrary/Activity.java
@@ -0,0 +1,16 @@
+package com.xx.mylibrary;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * FileName: Activity
+ * Author: xiongxiang
+ * Date: 2021/5/24
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+public class Activity  extends AppCompatActivity {
+
+}
---
+++ mylibrary/src/main/java/com/xx/mylibrary/Activity.java
@@ -0,0 +1,13 @@
+package com.xx.mylibrary;
+
+/**
+ * FileName: Activity
+ * Author: xiongxiang
+ * Date: 2021/5/24
+ * Description:
+ * History:
+ * <author> <time> <version> <desc>
+ * 作者姓名 修改时间 版本号 描述
+ */
+public class Activity extends android.app.Activity {
+}
